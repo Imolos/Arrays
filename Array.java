dc032334c85893922a498248221c7ff9b4722bda
Václav Lux <pumlicek@gmail.com>
2023-05-26 15:33:29 +0200

    First push

---
+++ Array.java
@@ -0,0 +1,7 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] Weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        System.out.println(Weekdays.length);
+        System.out.println(Weekdays[3]);
+    }
+}
