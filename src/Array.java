7d5e609a677a6828f85ab13d5fad5738233e0f8f
Václav Lux <pumlicek@gmail.com>
2023-05-26 19:52:22 +0200

    more Exercise

--- src/Array.java
+++ src/Array.java
@@ -1,9 +1,25 @@
 package src;
 
 public class Array {
+    //5
+    public void Argument(String... Argument) {
+
+    }
+
     public static void main(String[] args) {
+        //2
         String[] Weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
         System.out.println(Weekdays.length);
         System.out.println(Weekdays[3]);
+        //3
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //4
+        String[][] TicTacToe = {{"O","X","X"},{"X","O","O"},{"X","O","O"}};
+        System.out.println(TicTacToe[2][2]);
+
+
+
+
+
     }
 }
